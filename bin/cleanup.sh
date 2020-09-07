372ed792ecb5d0fca2a095f4e08cf401f5a7dcdf
Bradley Frank <bradfrank@fastmail.com>
2020-09-07 16:41:09 -0400

    Removed helper scripts; pushed Docker image.
    The oreilly app container is pushed to GitHub registry and the
    docker-compose updated to reflect. This also removes the need
    for the helper scripts so they are deleted.

--- bin/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker stop oreilly postgres adminer
-docker container rm oreilly postgres adminer
-docker rmi oreilly:latest postgres:9.6 adminer:latest python:3.8-slim
-docker network rm oreilly_oreilly
